67b22cc6b94cac3490872ecf26a686ad47c8797f
unknown <kamil.kamil.1995@wp.pl>
2024-03-17 15:50:39 +0100

    style entry page

--- src/compontents/entryScreen/EntryScreen.tsx
+++ src/compontents/entryScreen/EntryScreen.tsx
@@ -7,41 +7,64 @@ import Image from "next/image";
 const EntryScreen = () => {
   return (
     <>
-      <div className={styles.container}>
-        <div className={styles.imageContainer}>
-          <Image
-            src="/logo.svg"
-            layout="responsive"
-            className={styles.image}
-            alt="logo"
-            width={340}
-            height={308}
-          />
-        </div>
-        <div>
-          <div className={styles.h1container}>
-            <h1>The latest news from around the world</h1>
+      <div className={styles.wrapperAll}>
+        <div className={styles.container}>
+          <div className={styles.imageContainer}>
+            <Image
+              src="/logo.svg"
+              layout="responsive"
+              className={styles.image}
+              alt="logo"
+              width={340}
+              height={308}
+            />
           </div>
-          <h3>Join today.</h3>
-          <div className={styles.buttonLoginWith}>Login with google</div>
-          <div className={styles.buttonLoginWith}>Login with apple</div>
-          <div className={styles.orContainer}>
-            <span className={styles.spanOr}></span>
-            <p>or</p>
-            <span className={styles.spanOr}></span>
+          <div className={styles.contentContainer}>
+            <div className={styles.h1container}>
+              <h1>The latest news from around the world</h1>
+            </div>
+            <h3>Join today.</h3>
+            <div className={styles.buttonLoginWith}>Login with google</div>
+            <div className={styles.buttonLoginWith}>Login with apple</div>
+            <div className={styles.orContainer}>
+              <span className={styles.spanOr}></span>
+              <p>or</p>
+              <span className={styles.spanOr}></span>
+            </div>
+            <button className={styles.buttonCreateAccount}>
+              Create account
+            </button>
+            <p className={styles.desc}>
+              By registering, you agree to the Terms of Use and Privacy Policy,
+              including the Cookie Policy.
+            </p>
+            <h4>Already have an account?</h4>
+            <button className={styles.buttonLogin}>
+              <p>Login</p>
+            </button>
           </div>
-          <button className={styles.buttonCreateAccount}>Create account</button>
-          <p className={styles.desc}>
-            By registering, you agree to the Terms of Use and Privacy Policy,
-            including the Cookie Policy.
-          </p>
-          <h4>Already have an account?</h4>
-          <button className={styles.buttonLogin}>
-            <p>Login</p>
-          </button>
         </div>
+        <footer>
+          <span>Informacje</span>
+          <span>Pobierz aplikację serwisu X</span>
+          <span>Centrum Pomocy</span>
+          <span>Zasady użytkowania</span>
+          <span>Polityka prywatności</span>
+          <span>Polityka dotycząca plików cookie</span>
+          <span>Dostępność Informacja o reklamach</span>
+          <span>Blog</span>
+          <span>Status</span>
+          <span>Praca</span>
+          <span>Zasoby marki</span>
+          <span>Reklamy</span>
+          <span>Marketing</span>
+          <span>X dla firm</span>
+          <span>Deweloperzy</span>
+          <span>Ścieżka</span>
+          <span>Ustawienia</span>
+          <span>© 2024 X Corp.</span>
+        </footer>
       </div>
-      <footer>links</footer>
     </>
   );
 };
