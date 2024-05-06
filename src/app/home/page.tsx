cad63d547d7b19efcfb5b6184541ed48bbdead16
Kamildeeal <kamil.kamil.1995@wp.pl>
2024-05-06 22:40:47 +0200

    add auth user to check if user is logged in to see home page

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -56,43 +56,67 @@ const HomePage = ({ newsResults }: any) => {
 
   return (
     <>
-      <div className={styles.container}>
-        <div className={styles.left_wing}>
-          <Sidebar />
+      {user ? (
+        <div className={styles.container}>
+          <div className={styles.left_wing}>
+            <Sidebar />
+          </div>
+          <div className={styles.center}>
+            <div className={styles.center_header}>
+              <p>HomePage</p>
+              <button
+                className={styles.center_button_logout}
+                onClick={() => {
+                  auth.signOut();
+                  setCurrentUser({
+                    email: "",
+                    uid: "",
+                    image: "",
+                    text: "",
+                    timestamp: "",
+                    firstName: "",
+                  });
+                  sessionStorage.removeItem("user");
+                  router.push("/");
+                }}
+              >
+                Logout
+              </button>
+            </div>
+            <MiddleTestInput />
+            {/* <MiddleInput /> */}
+            <MiddlePost />
+          </div>
+          <div className={styles.right_wing}>
+            <RightSideBar />
+          </div>
+
+          {/* Modal */}
+          <CommentModal />
         </div>
-        <div className={styles.center}>
-          <div className={styles.center_header}>
-            <p>HomePage</p>
+      ) : (
+        <div className={styles.auth_user}>
+          <h1>Login first to tweet</h1>
+          <div>
             <button
-              className={styles.center_button_logout}
+              className={styles.button_89}
               onClick={() => {
-                auth.signOut();
-                setCurrentUser({
-                  email: "",
-                  uid: "",
-                  image: "",
-                  text: "",
-                  timestamp: "",
-                  firstName: "",
-                });
-                sessionStorage.removeItem("user");
+                router.push("/signup");
+              }}
+            >
+              Create acount to join
+            </button>
+            <button
+              className={styles.button_89}
+              onClick={() => {
                 router.push("/");
               }}
             >
-              Logout
+              Entry page
             </button>
           </div>
-          <MiddleTestInput />
-          {/* <MiddleInput /> */}
-          <MiddlePost />
         </div>
-        <div className={styles.right_wing}>
-          <RightSideBar />
-        </div>
-
-        {/* Modal */}
-        <CommentModal />
-      </div>
+      )}
     </>
   );
 };
